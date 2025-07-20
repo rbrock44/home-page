ccf2dcb51a47b74b74908d33ce9dfa42c49f0c1c
R Brock <rbrock444@gmail.com>
2025-07-20 15:51:40 -0500

    Update concert.service.ts

--- src/app/services/concert.service.ts
+++ src/app/services/concert.service.ts
@@ -164,8 +164,6 @@ export class ConcertService {
   }
 
   private getList(): Concert[] {
-    return _.cloneDeep(this.concertList.forEach(concert => {
-      concert.attendees = concert.attendees.map(name => name.split(' ')[0]);
-    }));
+    return _.cloneDeep(this.concertList);
   }
 }
